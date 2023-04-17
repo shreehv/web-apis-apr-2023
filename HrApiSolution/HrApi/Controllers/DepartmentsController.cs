f55cd108e8fea4d559c04efeb8158a29d84b689b
Rupashree H Viswanath <Rupashree_H_Viswanath@progressive.com>
2023-04-17 13:55:39 -0400

    Commit

---
+++ HrApiSolution/HrApi/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace HrApi.Controllers;
+
+
+
+public class DepartmentsController : ControllerBase
+{
+    // GET /departments
+    [HttpGet("/departments")]
+    public async Task<ActionResult> GetDepartments()
+    {
+        return Ok();
+    }
+}
