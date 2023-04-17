7f9f6848cc57c07bf8a234401ae1ecc4cc4f5131
Rupashree H Viswanath <Rupashree_H_Viswanath@progressive.com>
2023-04-17 13:58:56 -0400

    commit first from Jeff

--- HrApiSolution/HrApi/Controllers/DepartmentsController.cs
+++ HrApiSolution/HrApi/Controllers/DepartmentsController.cs
@@ -1,17 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-
+﻿using HrApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HrApi.Controllers;
 
-
-
 public class DepartmentsController : ControllerBase
 {
-    // GET /departments
-    [HttpGet("/departments")]
-    public async Task<ActionResult> GetDepartments()
+    // GET /departments
+    [HttpGet("/departments")]
+    public async Task<ActionResult<DepartmentsResponse>> GetDepartments()
     {
-        return Ok();
+        var response = new DepartmentsResponse
+        {
+            Data = new List<DepartmentSummaryItem> {
+                new DepartmentSummaryItem { Id="1", Name = "Developers"},
+                new DepartmentSummaryItem { Id="2", Name ="Testers"}
+            }
+        };
+        return Ok(response);
     }
 }
--- HrApiSolution/HrApi/Program.cs
+++ HrApiSolution/HrApi/Program.cs
@@ -1,23 +1,49 @@
+using HrApi.Domain;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+// the default web application builder has about 190+ "Services" that do all the work in your API.
+
+
 
 // Add services to the container.
 
-//builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+var hrConnectionString = builder.Configuration.GetConnectionString("hr-data");
+
+if(hrConnectionString is null)
+{
+    throw new Exception("No Connection String for HR Database");
+}
+
+builder.Services.AddDbContext<HrDataContext>(options =>
+{
+    options.UseSqlServer(hrConnectionString);
+});
+
+
+// before the application is built is above here services.
 var app = builder.Build();
+// after the application is built, how do you want to handle HTTP requests and responses.
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Open API - the documentation.
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-//app.UseAuthorization();
+app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers(); // it is going to create a phone directory.
+// route table:
+    // if someone does a GET /deparments:
+            // create an instance of the DepartmentsController
+            // Call the GetDepartments method.
 
-app.Run();
+app.Run(); // Starting the web server, and "blocking here"
