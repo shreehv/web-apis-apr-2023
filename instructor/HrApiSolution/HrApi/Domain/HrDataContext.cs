2354d82ea997636061cd0c375746bad73680e5f7
JeffryGonzalez <jeff@hypertheory.com>
2023-04-17 13:51:01 -0400

    right after lunch

---
+++ instructor/HrApiSolution/HrApi/Domain/HrDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HrApi.Domain;
+
+public class HrDataContext : DbContext
+{
+    public HrDataContext(DbContextOptions<HrDataContext> options): base(options)
+    {
+        
+    }
+
+    // All of the entity classes it should track in the database
+    public DbSet<DepartmentEntity> Departments { get; set; }
+
+}
--- instructor/HrApiSolution/HrApi/Program.cs
+++ instructor/HrApiSolution/HrApi/Program.cs
@@ -1,3 +1,6 @@
+using HrApi.Domain;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 // the default web application builder has about 190+ "Services" that do all the work in your API.
 
@@ -9,6 +12,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var hrConnectionString = builder.Configuration.GetConnectionString("hr-data");
+
+if(hrConnectionString is null)
+{
+    throw new Exception("No Connection String for HR Database");
+}
+
+builder.Services.AddDbContext<HrDataContext>(options =>
+{
+    options.UseSqlServer(hrConnectionString);
+});
+
 
 // before the application is built is above here services.
 var app = builder.Build();
